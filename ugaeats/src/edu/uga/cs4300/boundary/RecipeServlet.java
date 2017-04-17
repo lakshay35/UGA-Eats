30b9e6f2b8733c8c1880b590441d5ff224d13fa6
BradThirteen <bkr02962@uga.edu>
2017-04-17 16:53:39 -0400

    Updated Recipeservlet.java to be able to display recipes on index.html and homepage.ftl

--- ugaeats/src/edu/uga/cs4300/boundary/RecipeServlet.java
+++ ugaeats/src/edu/uga/cs4300/boundary/RecipeServlet.java
@@ -1,9 +1,12 @@
 package edu.uga.cs4300.boundary;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Blob;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -12,15 +15,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 import javax.sql.rowset.serial.SerialBlob;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import com.google.gson.Gson;
 
 import edu.uga.cs4300.logiclayer.RecipeLogicImpl;
+import edu.uga.cs4300.objectlayer.Recipe;
 import edu.uga.cs4300.objectlayer.User;
 import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapperBuilder;
 import freemarker.template.SimpleHash;
+import sun.misc.BASE64Encoder;
 
 /**
  * Servlet implementation class RecipeServlet
@@ -58,7 +69,10 @@ public class RecipeServlet extends HttpServlet {
 		String signin = request.getParameter("signin");
 		String home = request.getParameter("home");
 		String viewRecipes = request.getParameter("viewRecipe");
-		String createNewRecipe = request.getParameter("createrecipe");
+		//String createNewRecipe = request.getParameter("createrecipe");
+		String createRecipe = request.getParameter("createButton");
+		String loadPopularRecipes = request.getParameter("loadPopular");
+		String viewRecipe = request.getParameter("loadRecipe");
 		
 		if (signup != null)
 		{
@@ -85,24 +99,150 @@ public class RecipeServlet extends HttpServlet {
 		{
 			viewRecipes(request, response);
 		}
-		else if (createNewRecipe != null)
+		else if (createRecipe != null)
+		{
+			showRecipeMaker(request, response);
+		}
+		else if (ServletFileUpload.isMultipartContent(request))
 		{
 			createNewRecipe(request, response);
+		}
+		else if (loadPopularRecipes != null)
+		{
+			getPopularRecipes(request, response);
+			/*RecipeLogicImpl temp = new RecipeLogicImpl();
+			List<Blob> temp2 = new ArrayList<Blob>();
+			temp2 = temp.getImages();
+			System.out.println(temp2.size());
+			Blob blob = temp2.get(0);
+			int blobLength;
+			byte[] blobArray;
+			String image = null;
+			try {
+				blobLength = (int) blob.length();
+				blobArray = blob.getBytes(1, blobLength);
+				BASE64Encoder encoder = new BASE64Encoder();
+				image = encoder.encode(blobArray);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Gson g = new Gson();
+			System.out.println(g.toJson(image));
+			response.setContentType("text/plain");
+			try {
+				response.getWriter().write(image);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}*/
+		}
+		else if (viewRecipe != null)
+		{
+			System.out.println(viewRecipe);
+			viewSelectedRecipe(request, response);
+		}
+	}
+
+	private void viewSelectedRecipe(HttpServletRequest request, HttpServletResponse response) {
+		String tempId = request.getParameter("id");
+		int id = Integer.parseInt(tempId);
+		RecipeLogicImpl userCtrl = new RecipeLogicImpl();
+		Recipe recipe = userCtrl.getRecipeById(id);
+		Gson g = new Gson();
+		System.out.println(g.toJson(recipe));
+		response.setContentType("application/json");
+		try {
+			response.getWriter().write(g.toJson(recipe));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void getPopularRecipes(HttpServletRequest request, HttpServletResponse response) {
+		RecipeLogicImpl userCtrl = new RecipeLogicImpl();
+		List<Recipe> recipes = new ArrayList<Recipe>();
+		recipes = userCtrl.getImages();
+		
+		Gson g = new Gson();
+		System.out.println(g.toJson(recipes));
+		response.setContentType("application/json");
+		try {
+			response.getWriter().write(g.toJson(recipes));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void showRecipeMaker(HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession(false);
+		if (session != null)
+		{
+			DefaultObjectWrapperBuilder df = new DefaultObjectWrapperBuilder(Configuration.VERSION_2_3_25);
+			SimpleHash root = new SimpleHash(df.build());
+			String templateName = "createrecipe.ftl";
+			process.processTemplate(templateName, root, request, response);
+		}
+		else
+		{
+			try {
+				response.sendRedirect("signin.html");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
 	private void createNewRecipe(HttpServletRequest request, HttpServletResponse response) {
-		String name = request.getParameter("recipename");
-		String[] ingredients = request.getParameterValues("ingredients");
-		String[] instructions = request.getParameterValues("steps");
-		String permission = request.getParameter("visibility");
-		byte[] image = request.getParameter("pic").getBytes();
-		Blob blob = null;
+		RecipeLogicImpl userCtrl = new RecipeLogicImpl();
+		InputStream fileContent = null;
+		String recipeName = null;
+		String permission = null;
+		List<String> ingredients = new ArrayList<String>();
+		List<String> instructions = new ArrayList<String>();
 		try {
-			blob = new SerialBlob(image);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			List<FileItem> formItems = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+			for (FileItem formItem : formItems)
+			{
+				if (formItem.isFormField())
+				{
+					String name = formItem.getFieldName();
+					
+					if (name.equals("recipename"))
+					{
+						recipeName = formItem.getString();
+						System.out.println("recipeName");
+					}
+					else if (name.equals("ingredients"))
+					{
+						ingredients.add(formItem.getString());
+					}
+					else if (name.equals("steps"))
+					{
+						instructions.add(formItem.getString());
+					}
+					else if (name.equals("visibility"))
+					{
+						permission = formItem.getString();
+					}
+				}
+				else
+				{
+					fileContent = formItem.getInputStream();
+				}
+			}
+		} catch (FileUploadException e) {
 			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		HttpSession session = request.getSession();
+		int user_id = (Integer)(session.getAttribute("id"));
+		int check = userCtrl.createRecipe(recipeName, ingredients, instructions, permission, user_id, fileContent);
+		if (check != 0)
+		{
+			
 		}
 	}
 
@@ -121,7 +261,7 @@ public class RecipeServlet extends HttpServlet {
 		}
 		else
 		{
-			HttpSession session2 = request.getSession(false);
+			HttpSession session2 = request.getSession();
 			DefaultObjectWrapperBuilder df = new DefaultObjectWrapperBuilder(Configuration.VERSION_2_3_25);
 			SimpleHash root = new SimpleHash(df.build());
 			String templateName = "viewrecipe.ftl";
@@ -144,7 +284,7 @@ public class RecipeServlet extends HttpServlet {
 		}
 		else
 		{
-			HttpSession session2 = request.getSession(false);
+			HttpSession session2 = request.getSession();
 			DefaultObjectWrapperBuilder df = new DefaultObjectWrapperBuilder(Configuration.VERSION_2_3_25);
 			SimpleHash root = new SimpleHash(df.build());
 			String templateName = "homepage.ftl";
@@ -206,8 +346,6 @@ public class RecipeServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 	}
-	
-
-		
 
 }
+
